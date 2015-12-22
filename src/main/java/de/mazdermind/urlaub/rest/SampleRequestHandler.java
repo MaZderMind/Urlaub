561db6cc3767ad87dd0cec4a2a77bb03bba87aec
MaZderMind <github@mazdermind.de>
2015-12-22 12:40:06 +0100

    restructure resource-class to be … valid

--- src/main/java/de/mazdermind/urlaub/rest/SampleRequestHandler.java
+++ src/main/java/de/mazdermind/urlaub/rest/SampleRequestHandler.java
@@ -1,10 +1,14 @@
 package de.mazdermind.urlaub.rest;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
+@Path("/test")
 public class SampleRequestHandler
 {
-	@Path("/test")
+	@GET
+	@Produces("text/plain")
 	public String printMessage()
 	{
 		return "Foo";
